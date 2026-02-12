d3645f2e675c1540b297dfa6ff51ac4dc0b6b465
heitor170 <algodaodoce801@gmail.com>
2026-02-12 20:55:39 -0300

    Add files via upload

---
+++ script_game1.js
@@ -0,0 +1,14 @@
+let dinheiro = 0;
+
+let click = 1;
+
+function incrementar() {
+    dinheiro = dinheiro + click; 
+    
+    let h1 = document.getElementById('contador');
+    h1.innerText = dinheiro;
+}
+
+function click1() {
+    click++;
+}
